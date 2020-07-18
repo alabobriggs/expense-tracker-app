6c4016ba7a4c60d95ae5dd8ce3354219c66133e3
alabobriggs <alabobriggs@gmail.com>
2020-07-19 00:26:12 +0100

    add new transaction class and list

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: '23fjslq',
+      title: 'New shoes',
+      amount: 4009,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '22fjslq',
+      title: 'weekly groceries',
+      amount: 4009,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     // below uses named arguments
