ec80f5265d9123fd8a44b04a165ecacd2cab90f1
alabobriggs <alabobriggs@gmail.com>
2020-07-20 19:02:26 +0100

    move applicaiton state to main widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,101 @@
+import 'package:expense/models/transaction.dart';
+import 'package:expense/widgets/new_transaction.dart';
 import 'package:expense/widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: '23fjslq',
+      title: 'New shoes',
+      amount: 3490,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '22fjslq',
+      title: 'weekly groceries',
+      amount: 1009,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction({String title, double amount}) {
+    final newTransaction = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTransaction);
+    });
+  }
+
+  void startNewAddTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          child: NewTransaction(_addNewTransaction),
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // below uses named arguments
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Expense tracker'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: Card(
-                  child: Text('Chart'),
-                  elevation: 5,
-                  color: Colors.blue,
+          appBar: AppBar(
+            title: Text('Expense tracker'),
+            actions: <Widget>[
+              Builder(
+                builder: (bCtx) => IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () => startNewAddTransaction(bCtx),
                 ),
-                width: double.infinity,
-              ),
-              UserTransactions()
+              )
             ],
-            crossAxisAlignment: CrossAxisAlignment.stretch,
           ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
-        ),
-      ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Card(
+                    child: Text('Chart'),
+                    elevation: 5,
+                    color: Colors.blue,
+                  ),
+                  width: double.infinity,
+                ),
+                UserTransactions(
+                  addNewTransaction: _addNewTransaction,
+                  userTransactions: _userTransactions,
+                )
+              ],
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+            ),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: Builder(builder: (bCtx) {
+            return FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => startNewAddTransaction(bCtx),
+            );
+          })),
     );
   }
 }
--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -3,47 +3,23 @@ import 'package:expense/widgets/new_transaction.dart';
 import 'package:expense/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
 
-class  UserTransactions extends StatefulWidget {
-  @override
-  _UserTransactionsState createState() => _UserTransactionsState();
-}
+class UserTransactions extends StatelessWidget {
+  final List<Transaction> userTransactions;
 
-class _UserTransactionsState extends State<UserTransactions> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: '23fjslq',
-      title: 'New shoes',
-      amount: 3490,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '22fjslq',
-      title: 'weekly groceries',
-      amount: 1009,
-      date: DateTime.now(),
-    ),
-  ];
+  final Function addNewTransaction;
 
-  void _addNewTransaction({String title, double amount}) {
-    final newTransaction = Transaction(
-      id: DateTime.now().toString(),
-      title: title,
-      amount: amount,
-      date: DateTime.now(),
-    );
-
-    setState(() {
-      _userTransactions.add(newTransaction);
-    });
-  }
+  UserTransactions({
+    @required this.userTransactions,
+    @required this.addNewTransaction,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        NewTransaction(_addNewTransaction),
+        NewTransaction(addNewTransaction),
         TransactionList(
-          _userTransactions,
+          userTransactions,
         ),
       ],
     );
