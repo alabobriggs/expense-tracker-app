ee554ca5d4f75220de75e0bf03653957dc25c46f
alabobriggs <alabobriggs@gmail.com>
2020-07-23 15:38:59 +0100

    add list builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -33,45 +33,28 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, idx) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          '\$${userTransactions[idx].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor,
-                          ),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 5,
+                  ),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: FittedBox(
+                          child: Text('\$${userTransactions[idx].amount}'),
                         ),
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
                       ),
-                      Column(
-                        children: <Widget>[
-                          Text(userTransactions[idx].title,
-                              style: Theme.of(context).textTheme.headline2),
-                          Text(
-                            DateFormat.yMMMd()
-                                .format(userTransactions[idx].date),
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey),
-                          ),
-                        ],
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      '${userTransactions[idx].title}',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(userTransactions[idx].date),
+                    ),
                   ),
                 );
               },
@@ -80,3 +63,45 @@ class TransactionList extends StatelessWidget {
     );
   }
 }
+// Card(
+//   child: Row(
+//     children: <Widget>[
+//       Container(
+//         child: Text(
+//           '\$${userTransactions[idx].amount.toStringAsFixed(2)}',
+//           style: TextStyle(
+//             fontWeight: FontWeight.bold,
+//             fontSize: 20,
+//             color: Theme.of(context).primaryColor,
+//           ),
+//         ),
+//         margin: EdgeInsets.symmetric(
+//           vertical: 10,
+//           horizontal: 15,
+//         ),
+//         decoration: BoxDecoration(
+//           border: Border.all(
+//             color: Theme.of(context).primaryColor,
+//             width: 2,
+//           ),
+//         ),
+//         padding: EdgeInsets.all(10),
+//       ),
+//       Column(
+//         children: <Widget>[
+//           Text(userTransactions[idx].title,
+//               style: Theme.of(context).textTheme.headline2),
+//           Text(
+//             DateFormat.yMMMd()
+//                 .format(userTransactions[idx].date),
+//             style: TextStyle(
+//                 fontSize: 18,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.grey),
+//           ),
+//         ],
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//       )
+//     ],
+//   ),
+// );
