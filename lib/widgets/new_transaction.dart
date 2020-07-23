a4cc76d883a92d09fe6ba580bba05f32f29e8728
alabobriggs <alabobriggs@gmail.com>
2020-07-23 16:32:38 +0100

    add date to chart logic

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -18,16 +18,20 @@ class _NewTransactionState extends State<NewTransaction> {
   DateTime _selectedDate;
 
   void _onSubmitHandler() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = _titleController.text;
     final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
     widget.addNewTransaction(
       title: enteredTitle,
       amount: enteredAmount,
+      date: _selectedDate
     );
 
     Navigator.of(context).pop();
